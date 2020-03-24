068baaa9ec791424c20f1a904e26bad2e00028dc
Sumit Kumar Pandit <sumit.pandit@gds.ey.com>
2020-03-24 12:11:42 +0530

    first remote push

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Aux from "../../../hoc/Auxiliary";
+
+const OrderSummary = props => {
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                
+            </ul>
+        </Aux>
+    );
+};
+
+export default orderSummary;
